ea01ad44bc1b7051dba697349728085e582113a9
ALLANOSAN <allanstryper@live.com>
2024-11-09 14:47:53 -0300

    update

--- Tela_de_carregamento.Designer.cs
+++ Tela_de_carregamento.Designer.cs
@@ -28,11 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(330, 191);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Tela_de_carregamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Tela_de_carregamento";
             this.Text = "Tela_de_carregamento";
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.ProgressBar progressBar1;
 
         #endregion
     }
